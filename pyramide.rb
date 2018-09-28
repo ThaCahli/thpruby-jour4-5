e2fe452ee5b20bd9cf332a7de35720cc6d4d4ae9
ThaCahli <43366819+ThaCahli@users.noreply.github.com>
2018-09-28 23:45:41 +0200

    "vendredi exercices incomplets"

---
+++ pyramide.rb
@@ -0,0 +1,11 @@
+=begin
+Reprendre le programme exo_20.rb et fais un programme pyramide.rb qui montera au lieu de descendre : 
+Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?
+> 5
+Voici la pyramide :
+    #
+   ##
+  ###
+ ####
+#####
+= end
